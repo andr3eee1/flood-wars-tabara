32f8afb5d3bde53d14e9eb6de1390ffb908a7570
TraianDanciu <traian.mihai.danciu@gmail.com>
2024-07-03 12:37:39 +0300

    Adaugat functia de mutare.

--- infinity-matrix-v1.c
+++ infinity-matrix-v1.c
@@ -14,6 +14,8 @@
 #define INFINIT 2147483647
 #define MAXDEPTH 30
 #define MAXTIME 980000 // 0.98 sec
+#define NCOADA 256
+#define NDIR 4
 
 struct timeval tv;
 int cont;
@@ -28,9 +30,10 @@ static inline long long checktime() {
   return ((long long)tv.tv_sec) * 1000000 + tv.tv_usec;
 }
 
-int mat[MAXN+2][MAXM+2];
+char mat[MAXN+2][MAXM+2];
 int n,m,scorej,scores;
 char juc,mutch;
+char viz[MAXN + 2][MAXN + 2];
 
 char mut[]={'@','#','+','*','.'};
 
@@ -49,6 +52,79 @@ static inline int min(int a, int b){
 
 static inline int abs_(int x){
   return (x<0?-x:x);
+}
+
+struct Coada {
+  int prim, ultim;
+  char coadal[NCOADA], coadac[NCOADA];
+} frontiera[2];
+int dist[2], arie[2], frontl[2], frontc[2];
+
+int dlin[NDIR] = {-1, 0, 1, 0}, dcol[NDIR] = {0, 1, 0, -1};
+
+// facem mutare in color pentru jucatorul juc
+void makeMove(int color, int juc) {
+  int oldultim, dir, lnou, cnou;
+
+  // copy paste, urat dar ajuta pentru a evita un test
+  oldultim = frontiera[juc].ultim;
+  if(juc == 0) { // jucatorul de jos
+    while(prim != oldultim) {
+      // luam prima pozitie din coada
+      l = frontiera[juc].coadal[frontiera[juc].prim];
+      c = frontiera[juc].coadac[frontiera[juc].prim];
+      frontiera[juc].prim = (frontiera[juc].prim + 1) % NCOADA;
+    
+      for(dir = 0; dir < NDIR; dir++) {
+        // coordonatele noi
+        lnou = l + dlin[dir];
+        cnou = c + dcol[dir];
+        
+        if(viz[lnou][cnou] == 0 && mat[lnou][cnou] == color) // daca nu e luata si e culoarea buna
+          viz[lnou][ncou] = 1;
+
+          // adaugam noua pozitie in coada
+          frontiera[juc].coadal[frontiera[juc].ultim] = lnou;
+          frontiera[juc].coadac[frontiera[juc].ultim] = cnou;
+          frontiera[juc].ultim = (frontiera[juc].ultim + 1) % NCOADA;
+
+          // actualizam scorurile
+          arie[juc]++;
+          dist[juc] = min(dist[juc], lnou - 1 + m - cnou);
+          frontl[juc] = min(frontl[juc], lnou);
+          frontc[juc] = max(frontc[juc], cnou);
+        }
+      }
+    }
+  } else { // jucatorul de sus
+    while(frontiera[juc].prim != oldultim) {
+      // luam prima pozitie din coada
+      l = frontiera[juc].coadal[frontiera[juc].prim];
+      c = frontiera[juc].coadac[frontiera[juc].prim];
+      frontiera[juc].prim = (frontiera[juc].prim + 1) % NCOADA;
+
+      for(dir = 0; dir < NDIR; dir++) {
+        // coordonatele noi
+        lnou = l + dlin[dir];
+        cnou = c + dcol[dir];
+        
+        if(viz[lnou][cnou] == 0 && mat[lnou][cnou] == color) { // daaca nu e luata si e culoarea buna        
+          viz[lnou][cnou] = 1;
+
+          // adaugam noua pozitie in coada
+          frontiera[juc].coadal[frontiera[juc].ultim] = lnou;
+          frontiera[juc].coadac[frontiera[juc].ultim] = cnou;
+          frontiera[juc].ultim = (frontiera[juc].ultim + 1) % NCOADA;
+
+          // actualizam scorurile
+          arie[juc]++;
+          dist[juc] = min(dist[juc], n - lnou + cnou - 1);
+          frontl[juc] = max(frontl[juc], lnou);
+          frontc[juc] = min(frontc[juc], cnou);
+        }
+      }
+    }
+  }
 }
 
 // TODO: de implementat evaluarea statica a tablei
@@ -70,8 +146,8 @@ int negamax(int depth,int alpha,int beta){
   if(cont&&killer[depth]>=0){
     icolor=killer[depth];
     if(mut[icolor]!=int2char[table[n][1]]&&mut[icolor]!=int2char[table[1][m]]){
-      //TODO: trebuie implementata mutarea rapida, care calculeaza toata evaluarea statica
-
+      makeMove(icolor, juc ^ (depth % 2)); // juc pentru par si juc^1 pentru impar
+      
       score=-negamax(depth+1,-beta,-alpha);
 
       if(score>alpha){
@@ -83,7 +159,7 @@ int negamax(int depth,int alpha,int beta){
   icolor=0;
   while(cont&&alpha<beta&&icolor<5){
     if(icolor!=killer[depth]&&mut[icolor]!=int2char[table[n][1]]&&mut[icolor]!=int2char[table[1][m]]){
-      //TODO: trebuie implementata mutarea rapida, care calculeaza toata evaluarea statica
+      makeMove(icolor, juc ^ (depth % 2)); // juc pentru par si juc^1 pentru impar
 
       score=-negamax(depth+1,-beta,-alpha);
 
@@ -101,6 +177,16 @@ int negamax(int depth,int alpha,int beta){
 int main(){
   int l,c,ljuc,cjuc,maxicolor;
   char ch,chjuc;
+
+  // initializari frontiera si scoruri
+  frontiera[0].prim = frontiera[1].prim = 0;
+  frontiera[0].ultim = frontiera[1].ultim = 1;
+  frontiera[0].coadal[0] = frontl[0] = n;
+  frontiera[0].coadac[0] = frontc[0] = 1;
+  frontiera[1].coadal[1] = frontl[1] = 1;
+  frontiera[1].coadac[1] = frontc[1] = m;
+  dist[0] = dist[1] = n + m - 2;
+  arie[0] = arie[1] = 1;
 
   for(l=0;l<5;l++){
     char2int[mut[l]]=l;
